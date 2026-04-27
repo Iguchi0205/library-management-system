c95296004fb7b52184cb66a162412084d377a389
Yuto Iguchi <xxxiguchixxx@icloud.com>
2026-04-27 22:38:57 +0900

    初コミット：HelloWorldクラスを追加

---
+++ src/com/portfolio/library/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.portfolio.library;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,Library Msnsgement System!");
+		System.out.println("ポートフォリオを開始します。");
+
+	}
+
+}
